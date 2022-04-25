8e6e63b10b080c3b043363eb1d3fc915b5d59ee6
chaitanyabluepineapple <chaitanya.mohite@bluepineapple.io>
2022-04-25 11:19:52 +0530

    -added first commit files

---
+++ pages/index.js
@@ -0,0 +1,68 @@
+import Head from 'next/head'
+import SearchBar from '../components/SearchBar'
+import Navbar from '../components/Navbar'
+import GithubUser from '../components/GithubUser'
+import { useState, useRef, useEffect } from 'react'
+import { Loading } from '../components/Loading'
+
+export default function Home() {
+  let API = 'https://api.github.com/users/octocat'
+
+  const userRef = useRef(null)
+  const [userName, setUserName] = useState('')
+  const [data, setData] = useState('')
+  const [isLoading, setLoading] = useState(false)
+
+  function handleClick() {
+    console.log("Input Box Value", userRef.current.value)
+    setUserName(userRef.current.value);
+  }
+
+  function handleKeyPress(e){
+    if(e.key ==='Enter'){
+      console.log("Search Button", userRef.current.value)
+      setUserName(userRef.current.value);
+    }
+  }
+  useEffect(() => {
+    setLoading(true)
+    if (userName) {
+      API = `https://api.github.com/users/${userName}`
+    }
+
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [userName]);
+
+  if(!data) (
+  <p>No Profile data.</p>
+  )
+
+  console.log("API Return Users Data", data)
+
+  return (
+    <div className="min-h-screen bg-gray-50 py-7 dark:bg-[#1e253f]">
+      <Head>
+        <title>GitHub User Finder App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      
+      {isLoading ? <Loading /> :
+      <>
+      <SearchBar
+        userName={userName}
+        handleClick={handleClick}
+        userRef={userRef}
+        handleKeyPress={handleKeyPress}
+      />
+      <GithubUser data={data} />
+      </>
+      }
+    </div>
+  )
+}
